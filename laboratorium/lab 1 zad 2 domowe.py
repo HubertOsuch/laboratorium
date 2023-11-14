fe64e1771354c92142834f42bf744491aa0d1592
Hubert Osuch <w69992@student.wsiz.edu.pl>
2023-11-14 21:14:40 +0100

    Lab 2 zad domowe pętla while

--- laboratorium/lab 1 zad 2 domowe.py
+++
@@ -1,5 +0,0 @@
-int(3.0)  # ta instrukacja konwertuje liczbę zmiennoprzecinkową (float 3.0) na liczbę całkowitą (int) czyli w tym przypadku na 3
-float(3)  # ta instrukacja konwertuje liczbę całkowitą 3 na liczbę zmiennoprzecinkową (float) czyli w tym przypadku na 3.0
-float("3")  # Ta instrukcja konwertuje łańcuch znaków zawierający liczbę "3" na liczbę zmiennoprzecinkową (float) przez co otrzymamy 3.0
-str(12.4)  # Ta instrukcja konwertuje liczbę zmiennoprzecinkową (float) 12.4 na łańcuch znaków (str) czyli otrzrymamy "12.4"
-bool(0)  # Ta instrukcja konwertuje liczbę całkowitą 0 na typ logiczny (bool). Wynik to False bo w pythonie 0 rozumiane jest jako fałsz
--- laboratorium/lab 2 zad 3 domowe (pętla while).py
+++
@@ -1,9 +0,0 @@
-while True:
-    liczba = int(input("Podaj liczbę całkowitą nie ujemną: "))
-
-    if liczba < 0:
-        print("Podano liczbę ujemną. Koniec pętli")
-        break
-    else:
-        print(f"Podano liczbę {liczba}")
-
